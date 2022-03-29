b2f840dfdc7e49bc65ee3db257196f1343eaea36
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-03-29 18:08:24 -0300

    feat: make a service, repository and test to calc total area

--- desafio_quality/src/main/java/br/com/meli/desafio_quality/repository/PropertyRepository.java
+++ desafio_quality/src/main/java/br/com/meli/desafio_quality/repository/PropertyRepository.java
@@ -1,8 +1,23 @@
 package br.com.meli.desafio_quality.repository;
 
 
+import br.com.meli.desafio_quality.entity.PropertyEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class PropertyRepository {
+    private List<PropertyEntity> propertyEntities = new ArrayList<>();
+
+    public PropertyEntity findById(Integer id) {
+        return propertyEntities.stream()
+                .filter(propertyEntity -> propertyEntity.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Propriedade não encontrada"));
+    }
+
 }
