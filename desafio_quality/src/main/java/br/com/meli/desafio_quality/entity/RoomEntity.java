bf90569c253ebd9b78544ea9bae2af8144f5eae8
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-29 16:30:08 -0300

    fix: improove of room entity

--- desafio_quality/src/main/java/br/com/meli/desafio_quality/entity/RoomEntity.java
+++ desafio_quality/src/main/java/br/com/meli/desafio_quality/entity/RoomEntity.java
@@ -6,12 +6,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class RoomEntity {
 
     private String roomName;
     private Double roomWidth;
     private Double roomLength;
+    private Double area;
+
+    public RoomEntity(String roomName, Double roomWidth, Double roomLength) {
+        this.roomName = roomName;
+        this.roomWidth = roomWidth;
+        this.roomLength = roomLength;
+        this.area = calculateArea();
+    }
+
+    private Double calculateArea() {
+        return this.roomLength * this.roomWidth;
+    }
 
 }
