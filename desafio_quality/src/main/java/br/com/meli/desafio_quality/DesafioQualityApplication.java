f23375e30b34302c4d8846e24466dea5c100e062
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-03-29 14:44:37 -0300

    config: desafio_quality

---
+++ desafio_quality/src/main/java/br/com/meli/desafio_quality/DesafioQualityApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.desafio_quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioQualityApplication.class, args);
+	}
+
+}
---
+++ desafio_quality/src/test/java/br/com/meli/desafio_quality/DesafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.desafio_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
