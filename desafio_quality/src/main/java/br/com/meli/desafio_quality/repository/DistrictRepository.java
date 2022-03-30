58f003f4e6406b0f0febd54a9ec4df586f46502d
MatheusGuerra2022 <100309963+MatheusGuerra2022@users.noreply.github.com>
2022-03-30 15:40:49 -0300

    US02/feature - Add improvement in the US02/feature

---
+++ desafio_quality/src/main/java/br/com/meli/desafio_quality/repository/DistrictRepository.java
@@ -0,0 +1,39 @@
+package br.com.meli.desafio_quality.repository;
+
+
+import br.com.meli.desafio_quality.dto.DistrictDTO;
+import br.com.meli.desafio_quality.entity.DistrictEntity;
+import br.com.meli.desafio_quality.entity.PropertyEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class DistrictRepository {
+
+
+    public List<DistrictEntity> districts = new ArrayList() {
+        {
+        add(new DistrictEntity("Alto Branco", BigDecimal.valueOf(10)));
+        add(new DistrictEntity("Centro", BigDecimal.valueOf(5)));
+        add(new DistrictEntity("Conceição", BigDecimal.valueOf(15)));
+        add(new DistrictEntity("Dinamerica", BigDecimal.valueOf(5)));
+        add(new DistrictEntity("Liberdade", BigDecimal.valueOf(10)));
+        }
+    };
+
+
+
+    public void add(DistrictEntity district){
+        districts.add(district);
+
+    }
+
+
+
+}
