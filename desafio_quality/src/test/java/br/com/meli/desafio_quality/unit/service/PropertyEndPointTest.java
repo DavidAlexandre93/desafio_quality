2881be96959dc8a708d2c2f1324c556e8fce6cda
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-03-30 17:06:54 -0300

    config: implements tests integration S02

--- desafio_quality/src/test/java/br/com/meli/desafio_quality/unit/service/PropertyEndPointTest.java
+++ desafio_quality/src/test/java/br/com/meli/desafio_quality/unit/service/PropertyEndPointTest.java
@@ -2,19 +2,24 @@ package br.com.meli.desafio_quality.unit.service;
 
 
 
-import org.junit.jupiter.api.TestInstance;
+import br.com.meli.desafio_quality.dto.DistrictDTO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.math.BigDecimal;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @EnableWebMvc
+
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PropertyEndPointTest {
@@ -22,10 +27,31 @@ public class PropertyEndPointTest {
     @Autowired
     private MockMvc mvc;
 
-    public void valorTotalPropriedade_shouldReturnTotalPropertyValor_whenByPropertyId() throws Exception{
+    @Autowired
+    private PropertyService propertyService;
 
-       mvc.perform(MockMvcRequestBuilders.post("/teste").contentType(MediaType.APPLICATION_JSON)
-               .content("")
-       ).andExpect(MockMvcResultMatchers.status().isCreated());
+    public void classTest() throws Exception{
+
+       mvc.perform(MockMvcRequestBuilders.post("/teste")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(""))
+               .andExpect(MockMvcResultMatchers.status().isCreated());
     }
+
+    @Test
+    public void valorPropriedade_shouldTrowNewError_whenInvalidValor(Integer id) throws Exception {
+
+        BigDecimal valorProperty = propertyService.calculatePrecoAreaTotal(id);
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/totalpropriedade", valorProperty.add(BigDecimal.valueOf(id)))
+        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        String jsonReturned = result.getResponse().getContentAsString();
+        DistrictDTO dto = new ObjectMapper().readValue(jsonReturned, DistrictDTO.class);
+        assertEquals(propertyService.calculatePrecoAreaTotal(id), dto.getValueDistrictM2());
+    }
+
+
+
+
 }
